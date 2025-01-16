ec27ee1c8042e82c594b3c169c85edc5000bbb0d
Bohdan Tolmachov <bogdantolmachevvv@gmail.com>
2025-01-16 23:41:02 +0100

    feat: plans modal window

--- src/app/subscriptions/page.tsx
+++ src/app/subscriptions/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { usePathname } from "next/navigation";
 import Header from "@/app/components/Header";
 import Footer from "@/app/components/Footer";
@@ -17,6 +17,9 @@ import {
   TableRow,
 } from "@mui/material";
 import Link from "next/link";
+import { MdOutlineDone, MdOutlineKeyboardArrowRight } from "react-icons/md";
+import { IoMdClose } from "react-icons/io";
+import { CiCircleInfo } from "react-icons/ci";
 
 const tabs = [
   { name: "Profile", href: "/profile", icon: <FiUser size={24} /> },
@@ -30,9 +33,31 @@ const tabs = [
   { name: "Guides", href: "/guides", icon: <GrBook size={24} /> },
 ];
 
+const plans = [
+  { name: "Quarterly", days: 90, price: 129, monthlyCost: 43 },
+  { name: "Half year", days: 180, price: 229, monthlyCost: 30 },
+  { name: "Year", days: 365, price: 329, monthlyCost: 27 },
+  { name: "New year", days: 45, price: 59, monthlyCost: 0 },
+];
+
 const Subscriptions = () => {
   const pathname = usePathname();
   const isActive = (href: string) => pathname === href;
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedPlan, setSelectedPlan] = useState("Quarterly");
+
+  const toggleModal = () => {
+    setIsModalOpen(!isModalOpen);
+    if (!isModalOpen) {
+      document.body.classList.add("no-scroll");
+    } else {
+      document.body.classList.remove("no-scroll");
+    }
+  };
+
+  useEffect(() => {
+    return () => document.body.classList.remove("no-scroll");
+  }, []);
 
   return (
     <div className="bg-black text-white">
@@ -59,7 +84,7 @@ const Subscriptions = () => {
               ))}
             </ul>
           </nav>
-          <section className="min-h-[700px] md:min-h-[770px] lg:min-h-[950px] w-full lg:w-[85%] bg-[--dark-gray] p-[16px] sm:p-[32px] rounded-[16px]">
+          <section className="min-h-[750px] w-full lg:w-[85%] bg-[--dark-gray] p-[16px] sm:p-[32px] rounded-[16px]">
             <div className="flex-col flex">
               <div className="flex items-center justify-center w-[48px] h-[48px] bg-[#2A2B32] rounded-[12px]">
                 <FaDollarSign size={24} />
@@ -84,7 +109,10 @@ const Subscriptions = () => {
                     1/2
                   </p>
                 </div>
-                <button className="h-[44px] bg-[--green] px-[14px] py-[20px] font-bold leading-[16px] tracking-[-1%] rounded-[12px] flex items-center justify-center mt-[40px] my-[60px]">
+                <button
+                  onClick={toggleModal}
+                  className="h-[44px] bg-[--green] px-[14px] py-[20px] font-bold leading-[16px] tracking-[-1%] rounded-[12px] flex items-center justify-center mt-[40px] my-[60px]"
+                >
                   See plans
                 </button>
               </div>
@@ -152,6 +180,165 @@ const Subscriptions = () => {
             </div>
           </section>
         </div>
+        {isModalOpen && (
+          <>
+            <div className="fixed inset-0 bg-black bg-opacity-40 z-40"></div>
+
+            <div className="fixed inset-0 flex items-center justify-center z-50">
+              <div className="h-[750px] sm:h-[930px] w-[96%] xl:w-[1040px] xl:h-[800px] bg-[#1C1E22] p-6 rounded-[16px] shadow-lg flex flex-col xl:flex-row gap-[10px] xl:gap-[72px] relative overflow-y-scroll">
+                <button
+                  className="top-10 absolute sm:top-5 right-5"
+                  onClick={toggleModal}
+                >
+                  <IoMdClose
+                    size={24}
+                    className="hover:text-[#9EA0A6] cursor-pointer"
+                  />
+                </button>
+                <div className="py-[20px] sm:p-0 w-[100%] flex flex-col gap-[20px] xl:w-[450px]">
+                  <p className="text-[#CBFF51] text-[14px] leading-[20px]">
+                    Plans
+                  </p>
+                  <p className="font-bold text-[22px] xl:text-[24px] text-[26px] leading-[36px] -tracking-[3%] w-[330px]">
+                    Get Unlimited Access to the Site Materials
+                  </p>
+                  <p className="font-semibold text-[14px] leading-[20px] text-[#949392]  xl:w-[450px]">
+                    You will also receive an invitation to a private channel and
+                    access.
+                  </p>
+                  <div>
+                    <div className="relative mb-5">
+                      <MdOutlineDone
+                        size={20}
+                        className="text-[#CBFF51] absolute top-0 left-0"
+                      />
+                      <p className="font-semibold text-[14px] leading-[20px] px-[25px]">
+                        Unlimited access to view guides
+                      </p>
+                    </div>
+                    <div className="relative">
+                      <MdOutlineDone
+                        size={20}
+                        className="text-[#CBFF51] absolute top-0 left-0"
+                      />
+                      <p className="font-semibold text-[14px] leading-[20px] px-[25px]">
+                        Private channel and access to a closed section with
+                        projects with maximum potential
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div className="w-[100%] bg-transparent xl:w-[450px]">
+                  <h2 className="text-[18px] leading-[20px] -tracking-[0.18px] font-bold pt-[10px] xl:pt-[36px] mb-[15px]">
+                    Select plan
+                  </h2>
+                  <ul className="flex flex-col gap-2">
+                    {plans.map((plan) => (
+                      <li
+                        key={plan.name}
+                        onClick={() => setSelectedPlan(plan.name)}
+                        className={`cursor-pointer p-4 rounded-[12px] flex justify-between items-center border-[1px] transition-all duration-300 ${
+                          selectedPlan === plan.name
+                            ? "border-[#436237] border-[1px] bg-[#1D2A19]"
+                            : "border-gray-700 hover:border-gray-500"
+                        }`}
+                      >
+                        <div className="flex items-center gap-4">
+                          <div
+                            className={`w-[24px] h-[24px] flex items-center justify-center rounded-full border-2 transition-all duration-300 ${
+                              selectedPlan === plan.name
+                                ? "border-[1px] border-[#73A304] bg-[#528E09]"
+                                : "border-gray-700"
+                            }`}
+                          >
+                            {selectedPlan === plan.name && (
+                              <MdOutlineDone size={20} />
+                            )}
+                          </div>
+                          <div className="flex flex-col sm:flex-row items-center sm:gap-[12px]">
+                            <p className="font-bold text-[14px]">{plan.name}</p>
+                            <p className="text-[14px] text-[#8E8E8E]">
+                              {plan.days} days
+                            </p>
+                          </div>
+                        </div>
+                        <div>
+                          {plan.monthlyCost !== 0 ? (
+                            <div className="flex flex-col sm:flex-row items-center gap-[1px] sm:gap-[12px]">
+                              <p className="text-[16px] leading-[18px] font-bold text-[#CBFF51]">
+                                ${plan.price}
+                              </p>
+                              <p className="text-[14px] leading-[15px] text-[#8E8E8E]">
+                                ${plan.monthlyCost}/month
+                              </p>
+                            </div>
+                          ) : (
+                            <p className="text-[16px] text-left leading-[18px] font-bold text-[#CBFF51] mr-[25px] sm:mr-[95px]">
+                              ${plan.price}
+                            </p>
+                          )}
+                        </div>
+                      </li>
+                    ))}
+                  </ul>
+                  <div className="mt-[25px]">
+                    <p className="mb-1 text-[14px] leading-[16px] font-semibold">
+                      Cupon
+                    </p>
+                    <div className="flex items-center gap-2">
+                      <input
+                        className="bg-[#212226] p-2 w-[200px] rounded-[12px] placeholder:text-[14px] placeholder:leading-[20px]"
+                        placeholder="Cupon code"
+                      />
+                      <button className="bg-[#333333] px-[10px] py-[12px] text-[#A4A4A4] font-bold text-[14px] leading-[16px] rounded-[10px]">
+                        Done
+                      </button>
+                    </div>
+                  </div>
+
+                  <hr className="my-[20px] sm:my-[45px] border-0 h-px bg-[#27292D]" />
+
+                  <div className="flex items-center justify-between">
+                    <div className="flex flex-col gap-1">
+                      <p className="text-[14px] leading-[20px] font-semibold">
+                        Amount Due
+                      </p>
+                      <p className="text-[24px] leading-[28px] font-bold">
+                        $129
+                      </p>
+                    </div>
+                    {/*<button className="flex items-center gap-1 rounded-[16px] py-[18px] pr-[16px] pl-[24px] bg-[#11CA00] font-semibold leading-[20px]">*/}
+                    {/*  Go to payment*/}
+                    {/*  <MdOutlineKeyboardArrowRight />*/}
+                    {/*</button>*/}
+                    <button className="flex items-center gap-1 rounded-[16px] py-[18px] pr-[16px] pl-[24px] bg-[#11CA00] font-semibold leading-[20px]">
+                      Top up balance
+                      <MdOutlineKeyboardArrowRight />
+                    </button>
+                  </div>
+
+                  <p className="text-[#8E8E8E] text-[14px] leading-[20px] mt-[26px]">
+                    <span className="text-[#FF6F6F]">
+                      Account balance: $0.00.
+                    </span>{" "}
+                    <span className="hidden xl:inline">
+                      There are not enough funds on your account to pay for the
+                      order.
+                    </span>
+                  </p>
+
+                  <div className="flex text-[#D7B5FF] bg-[#B030BE0A] gap-2 mt-4 rounded-[12px] p-[10px]">
+                    <CiCircleInfo size={24} />
+                    <p className="text-[13px] leading-[18px]">
+                      Tip: if you get blocked at the time of payment -{" "}
+                      <span className="text-[#E37DFF]">use a VPN</span>
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </>
+        )}
       </main>
 
       <Footer />
