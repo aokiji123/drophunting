d3215ccc00d2c44a569895b87dcfdc0371160708
Bohdan Tolmachov <bogdantolmachevvv@gmail.com>
2025-02-15 01:07:56 +0100

    api change

--- src/shared/api/axios.ts
+++ src/shared/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://app.esdev.fun",
+  baseURL: "https://app.esdev.tech",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
