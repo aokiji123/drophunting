0c9ed5310460b998202c622249477dd2e74cced0
Bohdan Tolmachov <bogdantolmachevvv@gmail.com>
2025-03-23 00:42:24 +0100

    red small chart pie

--- src/shared/components/SmallChartPie.tsx
+++ src/shared/components/SmallChartPie.tsx
@@ -7,8 +7,9 @@ type SmallChartPieProps = {
 export default function SmallChartPie({
   max,
   current,
-  color = "#393B41",
+  color,
 }: SmallChartPieProps) {
+  console.log(color);
   const percentage = Math.min(Math.max(current / max, 0), 1) * 100;
   const angle = (percentage / 100) * 360;
   const largeArcFlag = angle >= 180 ? 1 : 0;
@@ -20,7 +21,7 @@ export default function SmallChartPie({
       <svg width="20" height="20" viewBox="0 0 20 20" className="absolute">
         <circle cx="10" cy="10" r="10" fill={color ? color : "#393B41"} />
         {percentage === 100 ? (
-          <circle cx="10" cy="10" r="10" fill="#CBFF51" />
+          <circle cx="10" cy="10" r="10" fill={color ? color : "#CBFF51"} />
         ) : (
           <path
             d={`M10 10 L10 0 A10 10 0 ${largeArcFlag} 1 ${x} ${y} L10 10 Z`}
