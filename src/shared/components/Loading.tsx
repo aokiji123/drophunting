b3b71de3657fe0d861ada3d6128a5ad222e877ae
Bohdan Tolmachov <bogdantolmachevvv@gmail.com>
2025-03-24 21:08:15 +0100

    loading color fix

--- src/shared/components/Loading.tsx
+++ src/shared/components/Loading.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Loading = () => {
   return (
-    <div className="flex justify-center items-center h-screen bg-transparent">
-      <div className="w-12 h-12 border-4 border-gray-300 border-t-gray-600 rounded-full animate-spin"></div>
+    <div className="fixed top-0 left-0 right-0 bottom-0 z-[999] bg-[#101114] text-white min-h-screen flex items-center justify-center">
+      <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-[#CBFF51]"></div>
     </div>
   );
 };
